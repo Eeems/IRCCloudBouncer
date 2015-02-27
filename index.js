b6bac83306ac1d77ca6ebbb8afd846e45521ae79
Nathaniel van Diepen <eeems@omnimaga.org>
2015-02-27 16:50:51 -0700

    hot swapping

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-global.IRCCloud = require('./lib/irccloud.js');
-global.irc = new IRCCloud();
+var hotswap = require('hotswap'),
+	extend = require('extend'),
+	IRCCloud = require('./lib/irccloud.js'),
+	irc = new IRCCloud(),
+	swap = function(){
+		if(irc.session){
+			var s = irc.session;
+			irc.delete();
+			irc = new IRCCloud(s);
+		}else if(irc.delete){
+			irc.delete();
+			irc = new IRCCloud();
+		}
+		extend(global,{
+			IRCCloud: IRCCloud,
+			irc: irc
+		});
+	};
+hotswap.configure({
+	watch: true,
+	autoreload: true
+});
+hotswap.on('swap',function(module){
+	console.log('Hot swapped '+module);
+});
+hotswap.on('error',function(e){
+	console.error(e);
+});
+
+hotswap.on('swap',swap);
+
+swap();
 
 require('repl').start({
 	useGlobal: true
